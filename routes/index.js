d0b928bdd559872a4c6cefee18893113f5bc3c2a
Babak <babak.jahedmanesh@thenavpeople.co.uk>
2022-09-30 03:08:44 +0100

    body/header parsing and url synth. + proxy struct

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const creds = require("../credentials/credentials.json");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('index');
+router.all('*', function (req, res, next) {
+  console.log(`url:\n${req.url}\noriginal-url:\n${req.originalUrl}\nbase-url:\n${req.baseUrl}\nbody:\n${JSON.stringify(req.body, null, 2)}\nheaders:\n${JSON.stringify(req.headers, null, 2)}`);
+
+  const urlSplit = req.originalUrl.split('/');
+  if (urlSplit && urlSplit.length > 0) {
+    const urlRoute = urlSplit[1];
+    if (urlRoute) {
+      const targetUrlCred = creds.find(c => c['enpoint-route'] === urlRoute);
+      if (targetUrlCred) {
+
+        const targetUrl = [targetUrlCred['oAuth-credentials']['base-url'], ...urlSplit.splice(2)].join('/');
+        var targetHeaders = {...req.headers};
+        delete targetHeaders["authorization"];
+
+        res.send(`url:(${targetUrl})\nbody:${JSON.stringify(req.body, null, 2)}\nheaders:(${JSON.stringify(targetHeaders, null, 2)})`);
+        return;
+      }
+    }
+  }
+
+  res.send(`couldn't find the target.`);
 });
 
 module.exports = router;
